1848fa57e04a7880e159951e487cbd316c870957
yukirochi <justineabanilla81@gmail.com>
2026-05-28 00:50:08 +0800

    set up dbt pipeline

---
+++ travel_time_prediction/models/staging/stg_data.sql
@@ -0,0 +1,27 @@
+WITH data_raw AS (
+    SELECT * FROM {{ source('raw_data', 'raw_table') }}
+),
+
+cleaned AS (
+    SELECT
+        CONVERT_TIMEZONE('UTC', 'Asia/Manila', timestamp::timestamp) AS timestamp,
+        CASE EXTRACT(DAYOFWEEK_ISO FROM departure_time::timestamp)
+            WHEN 1 THEN 'monday'
+            WHEN 2 THEN 'tuesday'
+            WHEN 3 THEN 'wednesday'
+            WHEN 4 THEN 'thursday'
+            WHEN 5 THEN 'friday'
+            WHEN 6 THEN 'saturday'
+            WHEN 7 THEN 'sunday'
+            ELSE 'None'
+        END AS day,
+        departure_time,
+        arrival_time,
+        traffic_delay_in_seconds,
+        traffic_length_in_meters,
+        length_in_meters,
+        travel_time_in_seconds
+    FROM data_raw
+)
+
+SELECT * FROM cleaned
