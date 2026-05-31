cb753d847cf78489c7e41284c63a4cc8636c342e
yukirochi <justineabanilla81@gmail.com>
2026-05-31 20:56:41 +0800

    model

---
+++ model/predictor.py
@@ -0,0 +1,50 @@
+from snowflake_info import cur
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+from sklearn.metrics import mean_absolute_error
+import pandas as pd
+import matplotlib.pyplot as plt
+df = cur.execute("SELECT * FROM training_data.staging_mart.mart_data").fetch_pandas_all()
+
+# print(df.head())
+
+x = df[['DAY_OF_WEEK', 'DEPARTURE_TIME']]
+
+y = df['TRAVEL_TIME_IN_MINUTES']
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+
+regressor = DecisionTreeRegressor(max_depth=4, random_state=42)
+
+regressor.fit(X_train, Y_train)
+
+
+# plt.figure(figsize=(20,10))
+
+# plot_tree(regressor, 
+#           feature_names=['DAY_OF_WEEK', 'DEPARTURE_TIME'], 
+#           filled=True,
+#           fontsize=8,
+#           proportion=True,
+#           precision=2
+#           )
+
+
+# plt.savefig('decision_tree.png', dpi=300, bbox_inches='tight')
+
+# plt.show()
+
+
+predictions = regressor.predict(X_test)
+error = mean_absolute_error(Y_test, predictions)
+print(f"Mean Absolute Error: {error:.2f} minutes")
+
+input_trip = pd.DataFrame({
+    'DAY_OF_WEEK': [1],  # Monday
+    'DEPARTURE_TIME': [2.51] 
+})
+
+predicted_time = regressor.predict(input_trip)
+print(f"Predicted Travel Time: {predicted_time[0]:.2f} minutes")
